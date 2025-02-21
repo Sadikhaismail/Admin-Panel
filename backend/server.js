3beeb3bb228e33d6acb56fcacab623c6761e0cad
Sadikhaismail <ismailsadikha@gmail.com>
2025-02-21 00:27:39 -1100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./controllers/AdminController")
+const ProfileRoutes = require ("./controllers/ProfileController")
+
+dotenv.config();
+const app = express();
+
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://admin-panel-frontend-jcc0.onrender.com", 
+    credentials: true, 
+  })
+);
+
+app.use("/api/admin", authRoutes);
+app.use("/api/profile", ProfileRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
